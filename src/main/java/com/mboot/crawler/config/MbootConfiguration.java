4782f6994cf774fc727e13422589c1ce6de78231
salahatwa <salahsayedatwa@gmail.com>
2020-06-21 16:42:45 +0300

    adding thread concurrency

--- src/main/java/com/mboot/crawler/config/MbootConfiguration.java
+++ src/main/java/com/mboot/crawler/config/MbootConfiguration.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
@@ -14,13 +15,28 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class MbootConfiguration {
+
+	@Bean
+	public TaskExecutor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(8);
+		executor.setQueueCapacity(100);
+		executor.setKeepAliveSeconds(60);
+		executor.setThreadNamePrefix("auto-post.logThread-");
+		executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+		executor.setWaitForTasksToCompleteOnShutdown(true);
+		return executor;
+	}
 
 	@Bean
 	public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyManagementException {
